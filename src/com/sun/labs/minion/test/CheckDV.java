e52a405bb8ec84bd34ec60ef395f4343d80dc880
eelstretching <devnull@localhost>
2008-10-22 17:56:17 +0000

    Less output.

--- src/com/sun/labs/minion/test/CheckDV.java
+++ src/com/sun/labs/minion/test/CheckDV.java
@@ -7,6 +7,7 @@ package com.sun.labs.minion.test;
 
 import com.sun.labs.minion.SearchEngine;
 import com.sun.labs.minion.SearchEngineFactory;
+import com.sun.labs.minion.TermStats;
 import com.sun.labs.minion.indexer.entry.FieldedDocKeyEntry;
 import com.sun.labs.minion.indexer.entry.QueryEntry;
 import com.sun.labs.minion.indexer.partition.DiskPartition;
@@ -34,6 +35,7 @@ public class CheckDV {
         FieldedDocKeyEntry dke = (FieldedDocKeyEntry) e.getManager().getDocumentTerm(key);
         int docID = dke.getID();
         DiskPartition part = (DiskPartition) dke.getPartition();
+        System.out.format("part: %s docID: %d fieldID: %d\n", part, dke.getID(), fieldID);
         PostingsIteratorFeatures feat = new PostingsIteratorFeatures();
         feat.setFields(e.getManager().getMetaFile().getFieldArray(field));
         PostingsIterator pi = dke.iterator(feat);
@@ -41,6 +43,8 @@ public class CheckDV {
             int tid = pi.getID();
             int freq = pi.getFreq();
             QueryEntry term = part.getMainDictionary().get(tid);
+            TermStats ts = e.getTermStats(term.getName().toString());
+            System.out.format("stats: %s term freq: %d\n", ts, freq);
             PostingsIterator ti = term.iterator(feat);
             if(!ti.findID(docID)) {
                 System.err.format("Can't find term: %s\n", term.getName());
@@ -48,8 +52,6 @@ public class CheckDV {
                 int tfreq = ((FieldedPostingsIterator) ti).getFieldFreq()[fieldID];
                 if(tfreq != freq) {
                     System.err.format("Mis-matched count: %s dv count: %d term count: %d\n", term.getName(), freq, tfreq);
-                } else {
-                    System.out.format("Match: %s %d %d\n", term.getName(), freq, tfreq);
                 }
             }
         }
