025600be85d81b49cb2e81cafeee13487c6313dd
eelstretching <devnull@localhost>
2009-04-13 18:02:33 +0000

    Update to the multi-threaded query sample.

--- src/com/sun/labs/minion/samples/MTQuery.java
+++ src/com/sun/labs/minion/samples/MTQuery.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.sun.labs.minion.samples;
 
 import com.sun.labs.minion.ResultSet;
@@ -27,7 +26,7 @@ import java.util.logging.Logger;
 public class MTQuery implements Runnable {
 
     protected SearchEngine engine;
-    
+
     protected List<String> queries;
 
     protected int duration;
@@ -38,52 +37,62 @@ public class MTQuery implements Runnable {
 
     protected Logger logger;
 
-    public MTQuery(List<String> queries, SearchEngine engine, int duration, int index) {
+    protected NanoWatch nw;
+
+    public MTQuery(List<String> queries, SearchEngine engine, int duration,
+            int index) {
         this.queries = new ArrayList<String>(queries);
+        Collections.shuffle(this.queries);
         this.engine = engine;
         this.duration = duration;
         this.index = index;
         stop = System.currentTimeMillis() + duration * 1000;
         logger = Logger.getLogger(getClass().getName());
+        nw = new NanoWatch();
     }
 
     public void run() {
         int iter = 0;
-        NanoWatch nw = new NanoWatch();
         while(System.currentTimeMillis() < stop) {
-            Collections.shuffle(queries);
             int qn = 1;
             for(String q : queries) {
                 try {
                     nw.start();
                     ResultSet rs = engine.search(q);
                     nw.stop();
-                    logger.info(String.format("thread: %d %d/%d/%d query: %s %.2fms %d results",
-                            index, 
-                            iter, qn, queries.size(),
-                            q, nw.getLastTimeMillis(), rs.size()));
-                    qn++;
-                } catch (SearchEngineException see) {
+                    if(logger.isLoggable(Level.FINER)) {
+                        logger.finer(String.format(
+                                "thread: %d %d/%d/%d query: %s %.2fms %d results",
+                                index,
+                                iter, qn, queries.size(),
+                                q, nw.getLastTimeMillis(), rs.size()));
+                    }
+                } catch(SearchEngineException see) {
                     logger.severe("Exception during search: " + see);
                 }
-                if(qn % 20 == 0) {
+                if(qn % 50 == 0) {
+                    logger.fine(String.format(
+                            "thread: %d queries: %d average query time: %.2fms",
+                            index, nw.getClicks(), nw.getAvgTimeMillis()));
                     if(System.currentTimeMillis() >= stop) {
-                        logger.info(String.format(
-                                "thread: %d iter: %d average query time: %.2fms",
-                                index, iter, nw.getAvgTimeMillis()));
-                        return;
+                        break;
                     }
                 }
+                qn++;
             }
-            logger.info(String.format("thread: %d iter: %d average query time: %.2fms", index, iter, nw.getAvgTimeMillis()));
-            nw.reset();
+            logger.fine(String.format(
+                    "thread: %d iter: %d average query time: %.2fms", index,
+                    iter, nw.getAvgTimeMillis()));
             iter++;
         }
     }
 
     public static void usage() {
-        System.err.println(String.format("Usage: MTQuery -d <index dir> -q <query file> [-n <num threads>] [-t <dur in seconds>]"));
-        System.err.println(String.format(" num threads defaults to 4, duration to 300 seconds"));
+        System.err.println(
+                String.format(
+                "Usage: MTQuery -d <index dir> -q <query file> [-n <num threads>] [-t <dur in seconds>]"));
+        System.err.println(String.format(
+                " num threads defaults to 4, duration to 300 seconds"));
     }
 
     /**
@@ -106,7 +115,8 @@ public class MTQuery implements Runnable {
             }
         }
 
-        String flags = "d:q:n:t:";
+        Logger logger = Logger.getLogger(MTQuery.class.getName());
+        String flags = "d:q:n:t:l:";
         Getopt gopt = new Getopt(args, flags);
 
         int nThreads = 4;
@@ -126,6 +136,10 @@ public class MTQuery implements Runnable {
 
                 case 'q':
                     queriesFile = gopt.optArg;
+                    break;
+
+                case 'l':
+                    logger.setLevel(Level.parse(gopt.optArg));
                     break;
 
                 case 'n':
@@ -151,28 +165,46 @@ public class MTQuery implements Runnable {
         }
         in.close();
 
-        System.out.println(String.format("Running %d queries in %d threads", queries.size(), nThreads));
+        System.out.println(String.format("Running %d queries in %d threads",
+                queries.size(), nThreads));
 
         SearchEngine engine = SearchEngineFactory.getSearchEngine(indexDir);
 
-        try {
-            MTQuery[] mtqs = new MTQuery[nThreads];
-            Thread[] threads = new Thread[nThreads];
-            for(int i = 0; i < nThreads; i++) {
-                mtqs[i] = new MTQuery(queries, engine, duration, i);
-                threads[i] = new Thread(mtqs[i]);
-                threads[i].start();
-            }
+        ResultSet s = engine.search("the");
+        s.getResults(0, 10);
 
-            for(int i = 0; i < nThreads; i++) {
-                try {
-                    threads[i].join();
-                } catch(InterruptedException ie) {
+        try {
+            NanoWatch total;
+            if(nThreads == 1) {
+                MTQuery single = new MTQuery(queries, engine, duration, 0);
+                single.run();
+                total = single.nw;
+            } else {
+                MTQuery[] mtqs = new MTQuery[nThreads];
+                Thread[] threads = new Thread[nThreads];
+                for(int i = 0; i < nThreads; i++) {
+                    mtqs[i] = new MTQuery(queries, engine, duration, i);
+                    threads[i] = new Thread(mtqs[i]);
+                    threads[i].setName("mtquery-" + i);
+                    threads[i].start();
+                }
+
+                total = new NanoWatch();
+                for(int i = 0; i < nThreads; i++) {
+                    try {
+                        threads[i].join();
+                        total.accumulate(mtqs[i].nw);
+                    } catch(InterruptedException ie) {
+                    }
                 }
             }
+            logger.info(String.format(
+                    "threads: %d queries: %d average query time: %.2fms",
+                    nThreads, total.getClicks(),
+                    total.getAvgTimeMillis()));
+            logger.info(engine.getQueryStats().dump());
         } finally {
             engine.close();
         }
     }
-
 }
