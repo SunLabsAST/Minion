d707375798a05c1e7631711b94e5a534d3a12b62
Stephen Green <stephen.x.green@oracle.com>
2011-11-30 10:42:08 -0500

    Iterators weren't actually being tested.  Fixed the bug that came up when they were (not detecting the end of the dictionary.)

---
+++ test/com/sun/labs/minion/indexer/dictionary/DictionaryAndPostingsTest.java
@@ -0,0 +1,189 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sun.labs.minion.indexer.dictionary;
+
+import com.sun.labs.minion.indexer.entry.EntryFactory;
+import com.sun.labs.minion.indexer.entry.IndexEntry;
+import com.sun.labs.minion.indexer.postings.OccurrenceImpl;
+import com.sun.labs.minion.indexer.postings.Postings;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author stgreen
+ */
+public class DictionaryAndPostingsTest {
+
+    static File tmpDir;
+
+    static Logger logger = Logger.getLogger(DictionaryTest.class.getName());
+
+    static TestData all;
+
+    public DictionaryAndPostingsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        all = new TestData("wordfreq.gz");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private MemoryDictionary<String> generateDictionary(Postings.Type type, int nDocs) {
+        MemoryDictionary<String> md =
+                new MemoryDictionary<String>(new EntryFactory(type));
+        OccurrenceImpl o = new OccurrenceImpl();
+        for(int i = 0; i < nDocs; i++) {
+            int size = all.r.nextInt(4096) + 1;
+            o.setID(i+1);
+            logger.info(String.format("Doc: %d size: %d", i+1, size));
+            for(int j = 0; j < size; j++) {
+                String w = all.getNextWord();
+                IndexEntry e = md.put(w);
+                e.add(o);
+            }
+        }
+        return md;
+    }
+
+    @Test
+    public void testSingleIDDictionary() {
+        generateDictionary(Postings.Type.ID, 1);
+    }
+
+    @Test
+    public void testSingleIDFreqDictionary() {
+        generateDictionary(Postings.Type.ID_FREQ, 1);
+    }
+
+    @Test
+    public void testSmallIDDictionary() {
+        generateDictionary(Postings.Type.ID, 20);
+    }
+
+    @Test
+    public void testSmallIDFreqDictionary() {
+        generateDictionary(Postings.Type.ID_FREQ, 20);
+    }
+
+    static protected class WordFreq implements Comparable<WordFreq> {
+
+        public String word;
+
+        public long freq;
+
+        public WordFreq(String word, int freq) {
+            this.word = word;
+            this.freq = freq;
+        }
+
+        public int compareTo(WordFreq o) {
+            return (int) (freq - o.freq);
+        }
+    }
+
+    static protected class TestData {
+
+        List<WordFreq> words = new ArrayList<WordFreq>();
+
+        double[] model;
+
+        Random r = new Random();
+
+        long totalFreq;
+
+        public TestData(String resourceName) throws Exception {
+            InputStream is = DictionaryTest.class.getResourceAsStream(
+                    resourceName);
+            if(is == null) {
+                is = TestData.class.getResourceAsStream("/com/sun/labs/minion/indexer/dictionary/resource/"
+                        + resourceName);
+            }
+            if(is == null) {
+                throw new java.io.IOException(String.format(
+                        "Couldn\'t find resource %s",
+                        resourceName));
+            }
+            BufferedReader r;
+            if(resourceName.endsWith(".gz")) {
+                r = new BufferedReader(new InputStreamReader(new GZIPInputStream(
+                        is), "utf-8"));
+            } else {
+                r = new BufferedReader(new InputStreamReader(is, "utf-8"));
+            }
+            String w;
+            int n = 0;
+            while((w = r.readLine()) != null) {
+                n++;
+                String[] f = w.split("\\s+");
+                if(f.length != 2) {
+                    logger.warning(String.format("Bad line %d: %s", n, w));
+                    continue;
+                }
+
+                try {
+                    WordFreq wf = new WordFreq(f[0], Integer.parseInt(f[1]));
+                    words.add(wf);
+                    totalFreq += wf.freq;
+                } catch(NumberFormatException ex) {
+                    logger.warning(String.format("Bad number at %d: %s", n, w));
+                }
+                if(n % 1000 == 0) {
+                    logger.info(String.format("%d: %s", n, w));
+                }
+            }
+            r.close();
+            logger.info(String.format("sorting %d", words.size()));
+            Collections.sort(words);
+            logger.info(String.format("sorted %d", words.size()));
+            model = new double[words.size()];
+            //
+            // Normalize the frequency.
+            for(int i = 0; i < words.size(); i++) {
+                model[i] = words.get(i).freq / (double) totalFreq;
+            }
+        }
+
+        /**
+         * Gets the next word using a zipfian distribution.
+         * @return
+         */
+        public String getNextWord() {
+            double p = r.nextDouble();
+            for(int i = 0; i < model.length; i++) {
+                p -= model[i];
+                if(p <= 0) {
+                    return words.get(i).word;
+                }
+            }
+            return words.get(words.size() - 1).word;
+        }
+    }
+}
