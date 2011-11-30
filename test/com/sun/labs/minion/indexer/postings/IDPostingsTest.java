d991cf9576151fe0321f05dedbf477880a559b37
Stephen Green <stephen.x.green@oracle.com>
2011-11-30 10:41:16 -0500

    Refactor encoding and iterating.  make TestData only hold a single row.

--- test/com/sun/labs/minion/indexer/postings/IDPostingsTest.java
+++ test/com/sun/labs/minion/indexer/postings/IDPostingsTest.java
@@ -9,6 +9,7 @@ import com.sun.labs.minion.util.buffer.WriteableBuffer;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -37,12 +38,9 @@ public class IDPostingsTest {
 
     public IDPostingsTest() {
     }
-    private static TestData randomData = new TestData(32);
-
     private static String[] previousData = new String[]{
         "/com/sun/labs/minion/indexer/postings/resource/2.data.gz",
-        "/com/sun/labs/minion/indexer/postings/resource/1.data.gz",
-    };
+        "/com/sun/labs/minion/indexer/postings/resource/1.data.gz",};
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -66,7 +64,7 @@ public class IDPostingsTest {
     @Test
     public void testSimpleAdd() throws Exception {
         TestData simple = new TestData(new int[]{1, 4, 7, 10});
-        encodeData(simple, false);
+        encodeData(simple);
     }
 
     /**
@@ -75,9 +73,9 @@ public class IDPostingsTest {
     @Test
     public void testSimpleMultipleAdd() throws Exception {
         TestData simple = new TestData(new int[]{1, 1, 1,
-                    1, 4, 4, 4, 7, 7, 8,
-                    10, 11, 11, 17});
-        encodeData(simple, false);
+                                                 1, 4, 4, 4, 7, 7, 8,
+                                                 10, 11, 11, 17});
+        encodeData(simple);
     }
 
     /**
@@ -88,92 +86,65 @@ public class IDPostingsTest {
         IDPostings idp = new IDPostings();
         OccurrenceImpl o = new OccurrenceImpl();
         int[] ids = new int[]{1, 4, 7, 10};
-        for (int i : ids) {
+        for(int i : ids) {
             o.setID(i);
             idp.add(o);
         }
 
         WriteableBuffer[] buffs = idp.getBuffers();
         assertTrue(String.format("Wrong number of buffers: %d", buffs.length),
-                buffs.length == 2);
+                   buffs.length == 2);
         ReadableBuffer rb = buffs[0].getReadableBuffer();
 
         int n = rb.byteDecode();
         assertTrue(String.format("Wrong number of IDs: %d", n), n == ids.length);
         n = rb.byteDecode();
-        assertTrue(String.format("Wrong last ID: %d", n), n == ids[ids.length -
-                1]);
+        assertTrue(String.format("Wrong last ID: %d", n), n == ids[ids.length
+                                                                   - 1]);
         rb = buffs[1].getReadableBuffer();
 
         int prev = 0;
-        for (int i = 0; i < ids.length; i++) {
+        for(int i = 0; i < ids.length; i++) {
             int gap = rb.byteDecode();
             int curr = prev + gap;
             assertTrue(String.format(
                     "Incorrect ID at %d: %d should be %d, decoded: %d", i, curr,
                     ids[i], gap),
-                    curr == ids[i]);
+                       curr == ids[i]);
         }
     }
 
-    private void encodeData(TestData data, boolean dumpOnFailure) throws
-            Exception {
-        for (int i = 0; i < data.rawData.length; i++) {
-            IDPostings idp = new IDPostings();
-            OccurrenceImpl o = new OccurrenceImpl();
-            int[] curr = data.rawData[i];
-            logger.info(String.format("Encoding %d ids (%d unique)", curr.length,
-                    data.sets[i].size()));
-            for (int j = 0; j < curr.length; j++) {
-                try {
-                    o.setID(curr[j]);
-                    idp.add(o);
-                } catch (Exception ex) {
-                    if (dumpOnFailure) {
-                        fail(String.format(
-                                "Failed to encode random data. Data row %d, item %d See: %s",
-                                i, j,
-                                randomData.dump()));
-                    } else {
-                        fail(String.format(
-                                "Failed to encode random data. Data row %d, item %d",
-                                i, j));
-                    }
-                }
-            }
+    private IDPostings encodeData(TestData data) {
+        IDPostings idp = new IDPostings();
+        OccurrenceImpl o = new OccurrenceImpl();
+        logger.info(String.format("Encoding %d ids (%d unique)",
+                                  data.rawData.length,
+                                  data.unique.size()));
+        for(int i = 0; i < data.rawData.length; i++) {
+            o.setID(data.rawData[i]);
+            idp.add(o);
+        }
+        return idp;
+    }
 
-            PostingsIterator pi = idp.iterator(null);
-            Iterator<Integer> ui = data.sets[i].iterator();
+    private void testIteration(IDPostings idp, TestData data) {
 
-            assertNotNull("Null postings iterator", pi);
+        PostingsIterator pi = idp.iterator(null);
+        Iterator<Integer> ui = data.unique.iterator();
 
-            if (data.sets[i].size() != pi.getN()) {
-                if (dumpOnFailure) {
-                    fail(String.format(
-                            "Expected %d ids got %d data row: %d, see: %s",
-                            data.sets[i].size(), pi.getN(), i,
-                            randomData.dump()));
-                } else {
-                    fail(String.format("Expected %d ids got %d data row: %d",
-                            data.sets[i].size(), pi.getN(), i));
+        assertNotNull("Null postings iterator", pi);
 
-                }
-            }
+        if(data.unique.size() != pi.getN()) {
+            fail(String.format("Expected %d ids got %d",
+                               data.unique.size(), pi.getN()));
+        }
 
-            while (pi.next()) {
-                int expected = ui.next();
-                if (pi.getID() != expected) {
-                    if (dumpOnFailure) {
-                        fail(String.format(
-                                "Couldn't match id %d, got %d, data row %d see %s",
-                                expected, pi.getID(), i, randomData.dump()));
-                    } else {
-                        fail(String.format(
-                                "Couldn't match id %d, got %d, data row %d",
-                                expected, pi.getID(), i));
-
-                    }
-                }
+        while(pi.next()) {
+            int expected = ui.next();
+            if(pi.getID() != expected) {
+                fail(String.format(
+                        "Couldn't match id %d, got %d",
+                        expected, pi.getID()));
             }
         }
     }
@@ -182,28 +153,61 @@ public class IDPostingsTest {
      * Tests encoding our random data, dumping the data to a file if a failure occurs.
      * @throws Exception if there is an error
      */
-    @Test
-    public void randomAddTest() throws Exception {
-        encodeData(randomData, true);
+    private void randomAddTest(int n) throws Exception {
+        Random rand = new Random();
+        for(int i = 0; i < 128; i++) {
+            TestData r = null;
+            try {
+                r = new TestData(rand.nextInt(n) + 1);
+                IDPostings idp = encodeData(r);
+                testIteration(idp, r);
+            } catch(AssertionError ex) {
+                PrintWriter out = getTempWriter();
+                r.dump(out);
+                out.close();
+                throw (ex);
+            }
+        }
     }
+
+    @Test
+    public void smallRandomAddTest() throws Exception {
+        randomAddTest(16);
+    }
+
+    @Test
+    public void mediumRandomAddTest() throws Exception {
+        randomAddTest(256);
+    }
+
+    @Test
+    public void largeRandomAddTest() throws Exception {
+        randomAddTest(8192);
+    }
+
+    @Test
+    public void extraLargeRandomAddTest() throws Exception {
+        randomAddTest(1024*1024);
+    }
+
     /**
      * Tests encoding data that has had problems before, ensuring that we
      * don't re-introduce old problems.
      */
     @Test
     public void previousDataTest() throws Exception {
-        for (String s : previousData) {
+        for(String s : previousData) {
 
             InputStream pdis = getClass().getResourceAsStream(s);
-            if (pdis == null) {
-                logger.info(String.format("Can't find resource: %s", s));
+            if(pdis == null) {
                 continue;
             }
             logger.info(String.format("Testing data %s", s));
             GZIPInputStream gzis = new GZIPInputStream(pdis);
             TestData td = new TestData(gzis);
             gzis.close();
-            encodeData(td, false);
+            IDPostings idp = encodeData(td);
+            testIteration(idp, td);
         }
     }
 
@@ -221,84 +225,66 @@ public class IDPostingsTest {
     public void testIterator() {
     }
 
+    private PrintWriter getTempWriter() throws java.io.IOException {
+        File f = File.createTempFile("random", ".data");
+        return new PrintWriter(new FileWriter(f));
+    }
+
     private static class TestData {
 
-        int[][] rawData;
+        int[] rawData;
 
-        Set[] sets;
+        Set<Integer> unique;
 
         public TestData(int n) {
-            rawData = new int[n][];
-            sets = new Set[n];
+            rawData = new int[n];
+            unique = new LinkedHashSet<Integer>();
 
             //
             // Generate some random data.  We need to account for gaps of zero, so
             // keep track of the unique numbers with some sets.
             Random r = new Random();
-            for (int i = 0; i < rawData.length; i++) {
-                int s = r.nextInt(1024 * 8) + 1;
-                int[] a = new int[s];
-                LinkedHashSet<Integer> uniq = new LinkedHashSet<Integer>();
-                int prev = 0;
+            int prev = 0;
+            for(int i = 0; i < rawData.length; i++) {
 
                 //
                 // We'll use random gaps to make sure we get appropriately increasing
                 // postings.
-                for (int j = 0; j < a.length; j++) {
-                    a[j] = prev + r.nextInt(256) + 1;
-                    prev = a[j];
-                    uniq.add(prev);
-                }
-                rawData[i] = a;
-                sets[i] = uniq;
+                rawData[i] = prev + r.nextInt(256) + 1;
+                prev = rawData[i];
+                unique.add(prev);
             }
         }
 
         public TestData(int[] d) {
-            rawData = new int[1][];
-            rawData[0] = d;
-            sets = new Set[1];
-            sets[0] = new LinkedHashSet<Integer>();
-            for (int x : d) {
-                sets[0].add(x);
+            rawData = d;
+            unique = new LinkedHashSet<Integer>();
+            for(int x : d) {
+                unique.add(x);
             }
         }
 
         public TestData(InputStream s) throws java.io.IOException {
             BufferedReader r = new BufferedReader(new InputStreamReader(s));
-            List<int[]> l = new ArrayList<int[]>();
-            String line;
-            while ((line = r.readLine()) != null) {
-                String[] nums = line.split("\\s");
-                int[] curr = new int[nums.length];
-                for (int j = 0; j < curr.length; j++) {
-                    curr[j] = Integer.parseInt(nums[j]);
-                }
-                l.add(curr);
+            String line = r.readLine();
+            unique = new LinkedHashSet<Integer>();
+            if(line == null) {
+                rawData = new int[0];
+                return;
             }
-            rawData = new int[l.size()][];
-            sets = new Set[rawData.length];
-            for (int i = 0; i < l.size(); i++) {
-                rawData[i] = l.get(i);
-                sets[i] = new LinkedHashSet<Integer>();
-                for (int x : rawData[i]) {
-                    sets[i].add(x);
-                }
+            String[] nums = line.split("\\s");
+            rawData = new int[nums.length];
+            for(int i = 0; i < rawData.length; i++) {
+                rawData[i] = Integer.parseInt(nums[i]);
+                unique.add(rawData[i]);
             }
         }
 
-        public String dump() throws java.io.IOException {
-            File f = File.createTempFile("random", ".data");
-            PrintWriter out = new PrintWriter(new FileWriter(f));
-            out.println(rawData.length);
-            for (int i = 0; i < rawData.length; i++) {
-                for (int j = 0; j < rawData[i].length; j++) {
-                    out.format("%d ", rawData[i][j]);
-                }
-                out.println("");
+        public void dump(PrintWriter out) throws java.io.IOException {
+            for(int i = 0; i < rawData.length; i++) {
+                out.format("%d ", rawData[i]);
             }
-            out.close();
-            return f.toString();
+            out.println("");
         }
     }
 }
