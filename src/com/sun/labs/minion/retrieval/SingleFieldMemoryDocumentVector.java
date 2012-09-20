a6dce715eb39bbc293e2df2c3de1f7944638d898
Stephen Green <stephen.x.green@oracle.com>
2012-09-20 10:53:12 -0400

    Make sure we're getting the right kind of term stats when building document vectors.

--- src/com/sun/labs/minion/retrieval/SingleFieldMemoryDocumentVector.java
+++ src/com/sun/labs/minion/retrieval/SingleFieldMemoryDocumentVector.java
@@ -25,7 +25,6 @@ package com.sun.labs.minion.retrieval;
 
 import com.sun.labs.minion.DocumentVector;
 import com.sun.labs.minion.FieldInfo;
-import com.sun.labs.minion.QueryConfig;
 import com.sun.labs.minion.QueryStats;
 import com.sun.labs.minion.ResultSet;
 import com.sun.labs.minion.SearchEngine;
@@ -34,6 +33,7 @@ import com.sun.labs.minion.engine.SearchEngineImpl;
 import com.sun.labs.minion.indexer.DiskField;
 import com.sun.labs.minion.indexer.Field;
 import com.sun.labs.minion.indexer.Field.DictionaryType;
+import com.sun.labs.minion.indexer.Field.TermStatsType;
 import com.sun.labs.minion.indexer.MemoryField;
 import com.sun.labs.minion.indexer.dictionary.MemoryDictionary;
 import com.sun.labs.minion.indexer.entry.IndexEntry;
@@ -60,9 +60,11 @@ import java.util.logging.Logger;
  * @see CompositeDocumentVectorImpl for an implementation that can handle
  * features from multiple vectored fields.
  */
-public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector implements Serializable {
+public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector
+        implements Serializable {
 
-    private static final Logger logger = Logger.getLogger(SingleFieldMemoryDocumentVector.class.getName());
+    private static final Logger logger = Logger.
+            getLogger(SingleFieldMemoryDocumentVector.class.getName());
 
     /**
      * We'll need to send this along when serializing as we're doing our own
@@ -75,11 +77,16 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
      * The field from which this document vector was generated.
      */
     protected transient FieldInfo field;
-    
+
     /**
      * The memory field from which this document vector was generated.
      */
     protected transient MemoryField mf;
+
+    /**
+     * What kind of term stats we're using.
+     */
+    Field.TermStatsType termStatsType;
 
     public SingleFieldMemoryDocumentVector() {
     }
@@ -88,27 +95,29 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
      * Creates a single fielded document vector from the data stored in an
      * in-memory field.
      */
-    public SingleFieldMemoryDocumentVector(MemoryField mf, String key, SearchEngine engine) {
+    public SingleFieldMemoryDocumentVector(MemoryField mf, String key,
+                                           SearchEngine engine) {
         this.mf = mf;
         this.key = key;
         this.engine = (SearchEngineImpl) engine;
         this.field = mf.getInfo();
         wf = engine.getQueryConfig().getWeightingFunction();
         wc = engine.getQueryConfig().getWeightingComponents();
-        
+
         //
         // Get the dictionary from which we'll draw the vector and the one
         // from which we'll draw terms, then look up the document.
         MemoryDictionary<String> vecDict;
-        Field.TermStatsType termStatsType;
         if(mf.isStemmed()) {
             termStatsType = Field.TermStatsType.STEMMED;
-            vecDict = (MemoryDictionary<String>) mf.getDictionary(DictionaryType.STEMMED_VECTOR);
+            vecDict = (MemoryDictionary<String>) mf.
+                    getDictionary(DictionaryType.STEMMED_VECTOR);
         } else {
             termStatsType = Field.TermStatsType.RAW;
-            vecDict = (MemoryDictionary<String>) mf.getDictionary(DictionaryType.RAW_VECTOR);
+            vecDict = (MemoryDictionary<String>) mf.
+                    getDictionary(DictionaryType.RAW_VECTOR);
         }
-        
+
         //
         // Make sure we compute weights with the right term stats!
         wc.setTermStatsType(termStatsType);
@@ -119,9 +128,11 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
             v = new WeightedFeature[0];
             return;
         }
-        
-        DocumentVectorPostings dvp = (DocumentVectorPostings) vecEntry.getPostings();
-        v = dvp.getWeightedFeatures(vecEntry.getID(), field.getID(), null, wf, wc);
+
+        DocumentVectorPostings dvp = (DocumentVectorPostings) vecEntry.
+                getPostings();
+        v = dvp.getWeightedFeatures(vecEntry.getID(), field.getID(), null, wf,
+                                    wc);
         length = 0;
         for(WeightedFeature feat : v) {
             length += feat.getWeight() * feat.getWeight();
@@ -134,7 +145,7 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
     public Collection<FieldInfo> getFields() {
         return Collections.singletonList(field);
     }
-    
+
     @Override
     public DocumentVector copy() {
         SingleFieldMemoryDocumentVector ret = new SingleFieldMemoryDocumentVector();
@@ -162,8 +173,8 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
      * Calculates the dot product of this document vector with another.
      *
      * @param dvi another document vector
-     * @return the dot product of the two vectors (i.engine. the sum of the products
-     * of the components in each dimension)
+     * @return the dot product of the two vectors (i.engine. the sum of the
+     * products of the components in each dimension)
      */
     public float dot(SingleFieldMemoryDocumentVector dvi) {
         dvi.getFeatures();
@@ -184,16 +195,22 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
      */
     @Override
     public ResultSet findSimilar(String sortOrder, double skimPercent) {
-        return SingleFieldMemoryDocumentVector.findSimilar(engine, getFeatures(), 
-                                                        field, sortOrder, skimPercent, wf, wc);
+        return SingleFieldMemoryDocumentVector.
+                                                           findSimilar(engine,
+                                                                       getFeatures(),
+                                                                       field,
+                                                                       sortOrder,
+                                                                       skimPercent,
+                                                                       wf, wc, termStatsType);
     }
-    
-    protected static ResultSet findSimilar(SearchEngine e, 
-                                           WeightedFeature[] v, 
+
+    protected static ResultSet findSimilar(SearchEngine e,
+                                           WeightedFeature[] v,
                                            FieldInfo field,
-                                           String sortOrder, double skimPercent, 
-                                           WeightingFunction wf, 
-                                           WeightingComponents wc) {
+                                           String sortOrder, double skimPercent,
+                                           WeightingFunction wf,
+                                           WeightingComponents wc,
+                                           TermStatsType termStatsType) {
 
         QueryStats qs = new QueryStats();
         qs.queryW.start();
@@ -205,7 +222,7 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
             int nf = (int) Math.floor(skimPercent * v.length);
             PriorityQueue<WeightedFeature> wfq =
                     new PriorityQueue<WeightedFeature>(nf,
-                    WeightedFeature.INVERSE_WEIGHT_COMPARATOR);
+                                                       WeightedFeature.INVERSE_WEIGHT_COMPARATOR);
             for(WeightedFeature twf : v) {
                 if(wfq.size() < nf) {
                     wfq.offer(twf);
@@ -222,7 +239,7 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
         } else {
             sf = v;
         }
-        
+
         //
         // We now have sf, which is the (possibly skimmed) set of features
         // that we want to use for finding similar documents.  Let's go ahead
@@ -251,7 +268,7 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
             if(cdf == null) {
                 continue;
             }
-            
+
             ScoredQuickOr qor = new ScoredQuickOr(curr, 1024, true);
             qor.setQueryStats(qs);
             qor.addField(field);
@@ -262,7 +279,8 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
                     qor.addWeightOnly(f.getWeight());
                     continue;
                 }
-                TermStatsImpl tsi = cdf.getTermStats(f.getName());
+                TermStatsImpl tsi = cdf.getTermStats(f.getName(),
+                                                     termStatsType);
                 wf.initTerm(wc.setTerm(tsi));
                 PostingsIterator pi = entry.iterator(feat);
 
@@ -275,7 +293,7 @@ public class SingleFieldMemoryDocumentVector extends AbstractDocumentVector impl
                     qor.addWeightOnly(f.getWeight());
                 }
             }
-            
+
             //
             // Add the results for this partition into the list
             // of results.
